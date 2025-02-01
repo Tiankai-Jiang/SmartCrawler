ffc1d60d45ccf1ecef9e70cbeb3adacf8c033069
tiankai <tkjiang97@gmail.com>
2025-02-01 16:11:19 +0100

    fix_encoding

--- src/crawler.py
+++ src/crawler.py
@@ -15,10 +15,8 @@ def get_html(url: str) -> Optional[str]:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                     "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
         "Referer": "https://www.google.com/",
     }
     try:
@@ -32,7 +30,7 @@ def get_html(url: str) -> Optional[str]:
         session.mount("https://", adapter)
         session.mount("http://", adapter)
 
-        response = session.get(url, headers=headers,timeout=10)
+        response = session.get(url, headers=headers, timeout=10)
         response.raise_for_status()  # Raise error for bad status codes (4xx/5xx)
         return response.text
 
